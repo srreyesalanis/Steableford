3d5c8f316d0131dbf0eed52a9966fe9c0e7b3f43
Roberto Reyes <srreyesalanis@gmail.com>
2026-07-04 01:13:27 -0600

    Fix net/pts: plain markdown, no HTML in columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -328,11 +328,7 @@ def capture_scores_ui():
         )
         calc = sf.calc_hole(gross, par, course_hcp, hh)
         pts_color = "#2e7d32" if calc['points'] >= 2 else "#c62828"
-        c2.markdown(
-            f"Net&nbsp;<b>{calc['net']}</b>&nbsp;&nbsp;"
-            f"Pts&nbsp;<b style='font-size:1.1rem;color:{pts_color}'>{calc['points']}</b>",
-            unsafe_allow_html=True
-        )
+        c2.markdown(f"Net **{calc['net']}** &nbsp; Pts **{calc['points']}**")
         st.divider()
         scores_input[player["id"]] = {"player": player, "gross": gross, "calc": calc}
 
